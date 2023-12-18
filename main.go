de20ef70eff92510ed9b1e770b3f1e1818cd3232
BaimZ <45239428+BaimZ@users.noreply.github.com>
2023-12-18 20:32:35 +0000

    clear

--- main.go
+++ main.go
@@ -1,56 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-)
-
-type Person struct {
-	Name string
-	Age  int
-}
-
-var people []Person
-
 func main() {
-	http.HandleFunc("/people", peopleHandler)
-	http.HandleFunc("/health", healthCheckHandler)
-	log.Println("server started on port 8080")
-	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
-}
 
-func peopleHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		getPeople(w, r)
-	case http.MethodPost:
-		postPerson(w, r)
-	default:
-		http.Error(w, "invalid http method", http.StatusMethodNotAllowed)
-	}
-}
-
-func getPeople(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(people)
-	fmt.Fprintf(w, "get people: '%v'", people)
-}
-
-func postPerson(w http.ResponseWriter, r *http.Request) {
-	var person Person
-	err := json.NewDecoder(r.Body).Decode(&person)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	people = append(people, person)
-	fmt.Fprintf(w, "person added: '%v'", person)
-}
-
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "health check")
 }
